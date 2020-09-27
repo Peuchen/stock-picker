5b58854df9aa6567b4c45f601e9a5881efcf24dd
peuchen <npeuchen@gmail.com>
2020-09-27 20:36:09 +0200

    Finish pseudocode

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,3 +1,6 @@
 #Create a method definiton for stock_picker
 #Go through each day
-#Check the difference between that day and every next day
+#Calculate the difference (profit) between that day and every next day
+#Check if the combination of days is the highest thus far
+#If so, store the combination of days in one variable and the profit in another variable
+#After checking all days, return the combination of days
