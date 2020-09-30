587a26b1b976b1fe9506aa28b7e3a6c6e4bf78b9
peuchen <npeuchen@gmail.com>
2020-09-30 18:55:46 +0200

    Add if statement for storing highest profit in variable

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,14 +1,20 @@
 #Create a method definiton for stock_picker
 def stock_picker(prices)
-#Create a variable to store the highest profit thus far
-  highest_profit = 0
-
-#Go through each day
-
+  #Create a variable to store the highest profit thus far
+  #Go through each day
 end
 #Calculate the difference (profit) between that day and every next day
+
 #Check if the combination of days is the highest thus far
-#If so, store the combination of days in one variable and the profit in another variable
+highest_profit = 0
+
+if profit > highest_profit
+  #If so, store the profit in one variable
+  highest_profit = profit
+  #and the best combination of days on another variable
+
+end
+
 #After checking all days, return the combination of days
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
