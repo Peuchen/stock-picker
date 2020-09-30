3c21865d7497fbaf09137a93c27587ce0f89b5f6
peuchen <npeuchen@gmail.com>
2020-09-30 20:50:26 +0200

    Calculate the profit for each day

--- stock-picker.rb
+++ stock-picker.rb
@@ -2,20 +2,32 @@
 def stock_picker(prices)
   #Create a variable to store the highest profit thus far
   #Go through each day
-end
+
 #Calculate the difference (profit) between that day and every next day
+  profit = 0
+  buy_day = 0
+  while buy_day < prices.length
+    sell_day = buy_day + 1
+    while sell_day < prices.length
+      profit = prices[sell_day] - prices[buy_day]
+      sell_day += 1
+    end
+    buy_day += 1
+  end
+
 
 #Check if the combination of days is the highest thus far
-highest_profit = 0
+  highest_profit = 0
 
-if profit > highest_profit
-  #If so, store the profit in one variable
-  highest_profit = profit
-  #and the best combination of days on another variable
+  if profit > highest_profit
+    #If so, store the profit in one variable
+    highest_profit = profit
+    #and the best combination of days on another variable
 
-end
+  end
 
 #After checking all days, return the combination of days
+end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
 
