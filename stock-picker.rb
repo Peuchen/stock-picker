533557d1fa95cec090c6dba8999974f78d3b3798
peuchen <npeuchen@gmail.com>
2020-09-30 21:09:51 +0200

    Combining setting variables with checking profits

--- stock-picker.rb
+++ stock-picker.rb
@@ -5,29 +5,29 @@ def stock_picker(prices)
 
 #Calculate the difference (profit) between that day and every next day
   profit = 0
+  highest_profit = 0
   buy_day = 0
+  day_combo = []
   while buy_day < prices.length
     sell_day = buy_day + 1
     while sell_day < prices.length
       profit = prices[sell_day] - prices[buy_day]
+      if profit > highest_profit
+        #If so, store the profit in one variable
+        highest_profit = profit
+        #and the best combination of days on another variable
+        day_combo = [buy_day, sell_day]
+      end
       sell_day += 1
     end
     buy_day += 1
   end
 
-
 #Check if the combination of days is the highest thus far
-  highest_profit = 0
-  if profit > highest_profit
-    #If so, store the profit in one variable
-    highest_profit = profit
-    #and the best combination of days on another variable
-    day_combo = [buy_day, sell_day]
-  end
-
+  day_combo
 #After checking all days, return the combination of days
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([47,3,6,9,15,8,6,19,7])
 
 #Maybe try it with loops instead of iterators
